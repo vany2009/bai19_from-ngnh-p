abdb9d3d31fbfe8fb09ad63c015d1600995fc488
Vany2009 <phanthilieu11071983@gmail.com>
2025-03-10 08:49:50 +0700

    thêm

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,44 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtmatkhau = new System.Windows.Forms.TextBox();
-            this.txtđăngnhâp = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
             this.btnđăngnhap = new System.Windows.Forms.Button();
             this.btnthoat = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.errorProviderUser = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderpass = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lblInfor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderpass)).BeginInit();
             this.SuspendLayout();
             // 
-            // txtmatkhau
+            // txtpass
             // 
-            this.txtmatkhau.Location = new System.Drawing.Point(160, 141);
-            this.txtmatkhau.Name = "txtmatkhau";
-            this.txtmatkhau.Size = new System.Drawing.Size(164, 20);
-            this.txtmatkhau.TabIndex = 0;
+            this.txtpass.Location = new System.Drawing.Point(160, 141);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(164, 20);
+            this.txtpass.TabIndex = 2;
             // 
-            // txtđăngnhâp
+            // txtuser
             // 
-            this.txtđăngnhâp.Location = new System.Drawing.Point(160, 91);
-            this.txtđăngnhâp.Name = "txtđăngnhâp";
-            this.txtđăngnhâp.Size = new System.Drawing.Size(164, 20);
-            this.txtđăngnhâp.TabIndex = 0;
+            this.txtuser.Location = new System.Drawing.Point(160, 91);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(164, 20);
+            this.txtuser.TabIndex = 1;
             // 
             // btnđăngnhap
             // 
             this.btnđăngnhap.Location = new System.Drawing.Point(105, 268);
             this.btnđăngnhap.Name = "btnđăngnhap";
             this.btnđăngnhap.Size = new System.Drawing.Size(112, 46);
-            this.btnđăngnhap.TabIndex = 1;
+            this.btnđăngnhap.TabIndex = 3;
             this.btnđăngnhap.Text = "Đăng nhập";
             this.btnđăngnhap.UseVisualStyleBackColor = true;
+            this.btnđăngnhap.Click += new System.EventHandler(this.btnđăngnhap_Click);
             // 
             // btnthoat
             // 
             this.btnthoat.Location = new System.Drawing.Point(268, 268);
             this.btnthoat.Name = "btnthoat";
             this.btnthoat.Size = new System.Drawing.Size(108, 46);
-            this.btnthoat.TabIndex = 1;
+            this.btnthoat.TabIndex = 4;
             this.btnthoat.Text = "Thoát";
             this.btnthoat.UseVisualStyleBackColor = true;
             // 
@@ -96,20 +104,42 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Đăng nhập hệ thống";
             // 
+            // errorProviderUser
+            // 
+            this.errorProviderUser.ContainerControl = this;
+            // 
+            // errorProviderpass
+            // 
+            this.errorProviderpass.ContainerControl = this;
+            // 
+            // lblInfor
+            // 
+            this.lblInfor.AutoSize = true;
+            this.lblInfor.Location = new System.Drawing.Point(112, 346);
+            this.lblInfor.Name = "lblInfor";
+            this.lblInfor.Size = new System.Drawing.Size(105, 13);
+            this.lblInfor.TabIndex = 2;
+            this.lblInfor.Text = "Đăng nhập hệ thống";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(772, 450);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblInfor);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnthoat);
             this.Controls.Add(this.btnđăngnhap);
-            this.Controls.Add(this.txtđăngnhâp);
-            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.txtpass);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderpass)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,13 +147,16 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtmatkhau;
-        private System.Windows.Forms.TextBox txtđăngnhâp;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtuser;
         private System.Windows.Forms.Button btnđăngnhap;
         private System.Windows.Forms.Button btnthoat;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider errorProviderUser;
+        private System.Windows.Forms.ErrorProvider errorProviderpass;
+        private System.Windows.Forms.Label lblInfor;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,51 @@ namespace bai19_fromđăngnhâp
         {
             InitializeComponent();
         }
+
+        private void btnđăngnhap_Click(object sender, EventArgs e)
+        {
+            string u = "admin";
+            string p = "123";
+            if (txtuser.Text == u && txtpass.Text == p)
+            {
+                // hiện from chương trình chính
+                Mainfrom mainfrom = new Mainfrom();
+                this.Hide();
+                mainfrom.ShowDialog();
+                this.Close();
+
+            }
+            else
+            {
+                if (txtuser.TextLength == 0)
+                {
+                    // MessageBox.show("user không được để trống");
+                    errorProviderUser.SetError(txtuser, "không được để trống");
+                }
+                else
+                {
+                    errorProviderUser.Clear();
+
+                }
+                if (txtpass.TextLength == 0)
+                {
+                    errorProviderpass.SetError(txtpass, "không được để trống");
+                }
+                else
+                {
+                    errorProviderpass.Clear();
+                }
+                // MessageBox.show("đăng nhập thất bại");
+                lblInfor.Text = "sai tên mật khẩu";
+                    }
+                }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblInfor.Text = "";
+        }
     }
-}
+
+        }
+    
+
